d570e50a845fade65a5f7da961d8962ca33c9cad
Benjamin Armstrong <benjamin.armstrong92@gmail.com>
2016-05-26 15:37:40 -0500

    Updated

--- final/public/main.js
+++ final/public/main.js
@@ -1,17 +1,22 @@
+'use strict';
+/* global io */
+
 var socket = io('http://localhost:5000/');
 var messageFormEl = document.getElementById('messageForm');
 
 var getName = function(){
-  return prompt('Enter your name:', "");
+  return prompt('Enter your name:', '');
 };
 
 var stringToColor = function(str) {
   var hash = 0;
-  for(var i = 0; i < str.length; i++) {
+
+  for (var i = 0; i < str.length; i++) {
     hash = str.charCodeAt(i) + ((hash << 3) - hash);
   }
   var color = Math.abs(hash).toString(16).substring(0, 6);
-  return "#" + '000000'.substring(0, 6 - color.length) + color;
+
+  return '#' + '000000'.substring(0, 6 - color.length) + color;
 };
 
 var addMessage = function(name, message){ // Append Message to chat list
@@ -19,15 +24,18 @@ var addMessage = function(name, message){ // Append Message to chat list
   var chatEl = document.getElementById('chat');
 
   var newNameEl = $$$('div');
-  newNameEl.className = "col-md-3 text-md-right";
+
+  newNameEl.className = 'col-md-3 text-md-right';
 
   var boldNameEl = $$$('b');
+
   boldNameEl.textContent = name;
   boldNameEl.style.color = stringToColor(name);
   newNameEl.appendChild(boldNameEl);
 
   var newMessageEl = $$$('div');
-  newMessageEl.className = "col-md-9";
+
+  newMessageEl.className = 'col-md-9';
   newMessageEl.textContent = message;
 
   chatEl.appendChild(newNameEl);
@@ -38,7 +46,8 @@ var sendMessage = function(event){ // Event handler for submit of the message in
   event.preventDefault();
 
   var messageEl = document.getElementById('message');
-  if(messageEl.value){
+
+  if (messageEl.value){
     socket.emit('new-message', messageEl.value);
     messageEl.value = '';
   }
@@ -50,12 +59,12 @@ socket.on('messages', function(newMessage){
   addMessage(newMessage.name, newMessage.message);
 });
 
-socket.on('connect', function(){ // On connect set name and
-  let name;
+let name;
 
-  while(!name){ // Wait till you give me a real name. #GhettoCodeLife
+socket.on('connect', function(){ // On connect set name and
+  while (!name){ // Wait till you give me a real name. #GhettoCode4Life
     name = getName();
-  };
+  }
 
   socket.emit('set-name', name);
 });
--- final/server.js
+++ final/server.js
@@ -1,4 +1,5 @@
-"use strict";
+'use strict';
+
 const express = require('express');
 const r = require('rethinkdb');
 const Events = require('events');
@@ -11,24 +12,23 @@ const emitter = new Events();
 app.use(express.static('public'));
 
 r.connect({
-    db: 'chat'
+  db: 'chat'
 }).then(function(conn){
-
     // DATABASE HAS CONNECTED
-    const MessagesTable = r.table('messages');
+  const MessagesTable = r.table('messages');
 
-    MessagesTable.changes().run(conn, function(err, messages){
-      if(err) console.log(err);
-      messages.each(function(err, message){
-        if(err) console.log(err);
-        emitter.emit('messages', message.new_val);
-      });
+  MessagesTable.changes().run(conn, function(err, messages){
+    if (err) console.log(err);
+    messages.each(function(err, message){
+      if (err) console.log(err);
+      emitter.emit('messages', message.new_val);
     });
+  });
 
-    emitter.on('new-message', function(message){
+  emitter.on('new-message', function(message){
       // new-message Event triggers insert
-      MessagesTable.insert(message).run(conn)
-    });
+    MessagesTable.insert(message).run(conn);
+  });
 });
 
 const botMessage = function(message){ // Send message as Bot name
@@ -37,43 +37,44 @@ const botMessage = function(message){ // Send message as Bot name
     message: message,
     createdAt: r.now()
   });
-}
+};
 
 io.on('connection', function (socket) {
-    let name;
+  let name;
 
-    const messageHandler = function(message){
-      socket.emit('messages', message);
-    };
+  const messageHandler = function(message){
+    socket.emit('messages', message);
+  };
 
-    emitter.on('messages', messageHandler);
+  emitter.on('messages', messageHandler);
 
-    socket.on('disconnect', function(){
-      emitter.removeListener('messages', messageHandler); // Remove listener to prevent memory leak
-      botMessage(`${name} has left the building.`);
+  socket.on('disconnect', function(){
+    emitter.removeListener('messages', messageHandler); // Remove listener to prevent memory leak
+    botMessage(`${name} has left the building.`);
+  });
+
+  socket.on('set-name', function(nameVal){
+    if (!name && nameVal && nameVal !== 'Bot') { // Only can set your name once and not to bot
+      name = nameVal.toString().substr(0, 50); // Limit name length
+
+      botMessage(`Welcome to the chatroom, ${name}.`);
+    }
+  });
+
+  socket.on('new-message', function(message){
+    if (!name) return; // Can't send without a name.
+
+    const limitedMessage = message.toString().substr(0, 200); // Limit message length
+
+    emitter.emit('new-message', {
+      name: name,
+      message: limitedMessage,
+      createdAt: r.now()
     });
-
-    socket.on('set-name', function(nameVal){
-      if(!name && nameVal && nameVal != 'Bot') { // Only can set your name once and not to bot
-        name = nameVal.toString().substr(0, 50); // Limit name length
-
-        botMessage(`Welcome to the chatroom, ${name}.`);
-      }
-    });
-
-    socket.on('new-message', function(message){
-      if(!name) return; // Can't send without a name.
-
-      const limitedMessage = message.toString().substr(0, 200); // Limit message length
-
-      emitter.emit('new-message', {
-          name: name,
-          message: limitedMessage,
-          createdAt: r.now()
-      });
-    });
-
+  });
 
 });
+
+console.log('It\'s alive!!! http://localhost:5000/ ');
 
 server.listen(5000);
